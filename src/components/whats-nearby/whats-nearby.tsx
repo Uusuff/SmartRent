01bfc01d683ca5ba5b7aac6646f8f017e0a8011c
Viktoriia <viktoriamyhailiak@gmail.com>
2025-12-04 13:49:42 +0200

    fix apartment page

--- src/components/whats-nearby/whats-nearby.tsx
+++ src/components/whats-nearby/whats-nearby.tsx
@@ -9,95 +9,72 @@ import styles from './whats-nearby.module.scss';
 import { useTranslation } from 'react-i18next';
 import { MultiLangText } from '../../types/Apartment';
 
-const shopIcon = new URL(
-  '../../assets/icons/mapIcons/shops.svg',
-  import.meta.url,
-).href;
+const shopIcon = new URL('icons/mapIcons/shops.svg', import.meta.url).href;
 const shopIconActive = new URL(
-  '../../assets/icons/mapIcons/shops-active.svg',
+  'icons/mapIcons/shops-active.svg',
   import.meta.url,
 ).href;
 
 const restaurantIcon = new URL(
-  'assets/icons/mapIcons/restaurants.svg',
+  'icons/mapIcons/restaurants.svg',
   import.meta.url,
 ).href;
 const restaurantIconActive = new URL(
-  'assets/icons/mapIcons/restaurants-active.svg',
+  'icons/mapIcons/restaurants-active.svg',
   import.meta.url,
 ).href;
 
-const gymIcon = new URL('assets/icons/mapIcons/gym.svg', import.meta.url).href;
-const gymIconActive = new URL(
-  'assets/icons/mapIcons/gym-active.svg',
-  import.meta.url,
-).href;
+const gymIcon = new URL('icons/mapIcons/gym.svg', import.meta.url).href;
+const gymIconActive = new URL('icons/mapIcons/gym-active.svg', import.meta.url)
+  .href;
 
 const supermarketIcon = new URL(
-  '../../assets/icons/mapIcons/supermarkets.svg',
+  'icons/mapIcons/supermarkets.svg',
   import.meta.url,
 ).href;
 const supermarketIconActive = new URL(
-  '../../assets/icons/mapIcons/supermarkets-active.svg',
+  'icons/mapIcons/supermarkets-active.svg',
   import.meta.url,
 ).href;
 
-const transportIcon = new URL(
-  '../../assets/icons/mapIcons/transport.svg',
-  import.meta.url,
-).href;
-const transportIconActive = new URL(
-  '../../assets/icons/mapIcons/transport-active.svg',
-  import.meta.url,
-).href;
-
-const schoolIcon = new URL(
-  '../../assets/icons/mapIcons/school.svg',
-  import.meta.url,
-).href;
-const schoolIconActive = new URL(
-  '../../assets/icons/mapIcons/school-active.svg',
-  import.meta.url,
-).href;
-
-const barIcon = new URL('../../assets/icons/mapIcons/bars.svg', import.meta.url)
+const transportIcon = new URL('icons/mapIcons/transport.svg', import.meta.url)
   .href;
-const barIconActive = new URL(
-  '../../assets/icons/mapIcons/bars-active.svg',
+const transportIconActive = new URL(
+  'icons/mapIcons/transport-active.svg',
   import.meta.url,
 ).href;
 
-const hospitalIcon = new URL(
-  '../../assets/icons/mapIcons/hospital.svg',
+const schoolIcon = new URL('icons/mapIcons/school.svg', import.meta.url).href;
+const schoolIconActive = new URL(
+  'icons/mapIcons/school-active.svg',
   import.meta.url,
 ).href;
+
+const barIcon = new URL('icons/mapIcons/bars.svg', import.meta.url).href;
+const barIconActive = new URL('icons/mapIcons/bars-active.svg', import.meta.url)
+  .href;
+
+const hospitalIcon = new URL('icons/mapIcons/hospital.svg', import.meta.url)
+  .href;
 const hospitalIconActive = new URL(
-  '../../assets/icons/mapIcons/hospital-active.svg',
+  'icons/mapIcons/hospital-active.svg',
   import.meta.url,
 ).href;
 
-const parkIcon = new URL(
-  '../../assets/icons/mapIcons/parks.svg',
-  import.meta.url,
-).href;
+const parkIcon = new URL('icons/mapIcons/parks.svg', import.meta.url).href;
 const parkIconActive = new URL(
-  '../../assets/icons/mapIcons/parks-active.svg',
+  'icons/mapIcons/parks-active.svg',
   import.meta.url,
 ).href;
 
-const parkingIcon = new URL(
-  '../../assets/icons/mapIcons/parking-lots.svg',
-  import.meta.url,
-).href;
+const parkingIcon = new URL('icons/mapIcons/parking-lots.svg', import.meta.url)
+  .href;
 const parkingIconActive = new URL(
-  '../../assets/icons/mapIcons/parking-lots-active.svg',
+  'icons/mapIcons/parking-lots-active.svg',
   import.meta.url,
 ).href;
 
-const homeIcon = new URL(
-  '../../assets/icons/mapIcons/home.svg',
-  import.meta.url,
-).href;
+const homeIcon = new URL('icons/mapIcons/home.svg', import.meta.url).href;
 
 export type POI = {
   id: number;
